4efcca4a82b31c1c6ca883083c307205d92d8fda
Alfredo Barillas <Alfredoeb96@gmail.com>
2018-06-09 23:34:19 -0700

    webpack config
    HTML, CSS(SCSS), JavaScript build and production setup

---
+++ src/index.js
@@ -0,0 +1,5 @@
+console.log(`I've been required by Webpack`);
+const arr = [1, 2, 3];
+const iAmJavascriptES6 = () => console.log(...arr);
+window.iAmJavascriptES6 = iAmJavascriptES6;
+import style from "./scss/main.scss";
