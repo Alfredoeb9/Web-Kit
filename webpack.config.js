643323fbb349209d2b4c9fe8ded076dfc4527413
Alfredo Barillas <Alfredoeb96@gmail.com>
2018-07-04 21:12:38 -0700

    Update dist folders

--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,16 @@ const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 var path = require('path');
 
 
 module.exports = {
+  entry: { main: './src/index.js' },                    //  This controls src input to 
+  output: {                                             //  dist output. Change as you please.
+    path: path.resolve(__dirname, 'dist/'),
+    filename: 'js/[name].[chunkhash].js'                // Chunkhash is not needed but can help cache problems
+  },
   optimization: {
     minimizer: [
       new UglifyJsPlugin({
@@ -23,21 +29,22 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: "babel-loader"
-        }
+        },
+        
       },
       {
         test: /\.html$/,
-        use: [{ loader: "html-loader", options: { minimize: true } }]
+        use: [{ loader: "html-loader", options: { minimize: true } }]     //  Keeps this true for optimized HTML
       },
-      { //  Optimize Images
-        test: /\.(png|jpe?g)/i,
+      { //  Optimize Images 
+        test: /\.(png|jpe?g|svg|gif)/i,
         use: [
           {
             loader: "url-loader",
             options: {
               name: "./img/[name].[ext]",
-              limit: 10000                  //  Change limit for limit pic size into data string (faster reloading)
-            }
+              limit: 10000                //  Inline Img as bas64 if smaller than 10kb for faster loading 
+            }                             //  Highest recommended 12-15 kb
           },
           {
             loader: "img-loader"
@@ -47,15 +54,21 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
+          'style-loader',
           MiniCssExtractPlugin.loader,
-          "css-loader",
-          "postcss-loader",
-          "sass-loader"
+          "css-loader",                 //  Parses the CSS into JavaScript and resolves any dependencies
+          "postcss-loader",             //  Outputs autoprefixers in dist
+          "sass-loader"                 //  Outputs our CSS into a <style> tag in the header
         ]
       }
     ]
   },
   plugins: [
+    new CleanWebpackPlugin('dist', {} ),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].css",
+      chunkFilename: "[id].css"
+    }),
     new HtmlWebPackPlugin({
       template: "src/index.html",
       filename: "./index.html"
@@ -64,9 +77,6 @@ module.exports = {
       template: "src/index.html",
       filename: "./index2.html",      
     }),
-    new MiniCssExtractPlugin({
-      filename: "css/[name].css",
-      chunkFilename: "[id].css"
-    })
+    
   ]
-};
+}
